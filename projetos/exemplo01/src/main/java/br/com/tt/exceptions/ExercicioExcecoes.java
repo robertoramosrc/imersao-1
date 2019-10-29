b3ad54f773c4215c04686a858229352a5b8e9aaa
gilberto.lupatini <gilberto.lupatini@gmail.com>
2019-10-28 22:45:42 -0300

    Exercicio Coleções

---
+++ projetos/exemplo01/src/main/java/br/com/tt/exceptions/ExercicioExcecoes.java
@@ -0,0 +1,48 @@
+package br.com.tt.exceptions;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+
+public class ExercicioExcecoes {
+
+    public static void main(String args[]) throws Exception {
+        excecoesNumericas();
+        nullPointerException();
+        indexOfBounds();
+        arquivoInexistente();
+        erroDeConversao();
+        disparoDeExcecao();
+    }
+
+    private static void disparoDeExcecao() {
+        throw new IllegalArgumentException();
+    }
+
+    private static void erroDeConversao() {
+        int num = Integer.parseInt ("zero");
+        System.out.println(num);
+    }
+
+    private static void arquivoInexistente() throws FileNotFoundException {
+        File file = new File("E://arquivo.txt");
+        FileReader fr = new FileReader(file);
+    }
+
+    private static void indexOfBounds() {
+        String e = "Imersao Java";
+        char f = e.charAt(29);
+        System.out.println(e);
+    }
+
+    private static void nullPointerException() {
+        String d = null;
+        System.out.println(d.charAt(0));
+    }
+
+    private static void excecoesNumericas() {
+        int a = 30, b = 0;
+        int c = a/b;
+        System.out.println ("Resultado = " + c);
+    }
+}
